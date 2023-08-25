08879e75cd4da094d7a3ff01ac5169ca02afe5e8
bong <darksen7693@gmail.com>
2023-08-25 11:34:22 +0900

    user회원가입

--- src/main/java/shop/mtcoding/blogv2/user/UserController.java
+++ src/main/java/shop/mtcoding/blogv2/user/UserController.java
@@ -2,6 +2,7 @@ package shop.mtcoding.blogv2.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -10,11 +11,14 @@ public class UserController {
         return "redirect:/";
     }
 
-
     @GetMapping("/updateForm")
     public String update() {
         return "updateForm";
     }
 
+    @PostMapping("/join")
+    public String join() {
+        return "join";
+    }
 
 }
